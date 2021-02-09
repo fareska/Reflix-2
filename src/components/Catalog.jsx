7984b8886eeba90f3c0924dc9590ac5cfeb530cf
fareska <fares.xb@gmail.com>
2021-02-10 01:43:22 +0200

    displaying movies according to the search input

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -15,20 +15,49 @@ export default class Catalog extends Component {
         let value = e.target.value
         this.setState({
             searchVal: value
-        })
+        }) //,console.log(this.state)// this log the previous version of the searchVal !!??
     }
 
-    renderRentedMovies = () => {
+    searchedMovies = movies => {
+        let input = this.state.searchVal.toLowerCase()
 
+        let relevantMovies = movies.filter(m => m.title.toLowerCase().includes(input))
+        
+        console.log(movies, 'movies');
+        console.log(relevantMovies, 'relevantMovies');
+        console.log(input, 'input');
+        
+        return relevantMovies
     }
-
 
     render() {
 
         let rentedMovies = this.props.movies.filter(m => m.isRented === true)
+        rentedMovies = this.searchedMovies(rentedMovies) 
+        
+         
+        let movies = this.searchedMovies(this.props.movies) || this.props.movies 
         return (
             <div className='Catalog'>
+
                 <input name='searchVal' type="text" value={this.state.searchVal} placeholder='Search Movie' onChange={this.handleInput} />
+                <div className="input-field">
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <textarea id="textarea1" class="materialize-textarea"
+                                        style={{ color: 'white' }}
+                                        onChange={this.handleInput}
+                                        value={this.state.searchVal}
+                                    >
+                                    </textarea>
+                                    <label for="textarea1">Textarea</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
                 {rentedMovies.length > 0
                     && <div className="row">
                         <h3>Rented Movies:</h3>
@@ -45,7 +74,7 @@ export default class Catalog extends Component {
 
                 <div className="row">
                     <h3>Catalog:</h3>
-                    {this.props.movies.map((m, i) => {
+                    {movies.map((m, i) => {
                         return (
                             <div key={i} >
                                 <Movie id={m.id} movie={m} rentMovie={this.props.rentMovie} />
